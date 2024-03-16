ff39888d0df769da193580cedfe2f29078f16b8b
Shilov Stas <sofabb9@gmail.com>
2024-03-16 14:40:25 +0700

    Донастройка линтера

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
     	}],
 		"@typescript-eslint/method-signature-style": "off",
 		"@typescript-eslint/consistent-type-definitions": "off",
-		"@typescript-eslint/unbound-method": "off"
+		"@typescript-eslint/unbound-method": "off",
+    	"@typescript-eslint/no-confusing-void-expression": "off"
 	},
 };
